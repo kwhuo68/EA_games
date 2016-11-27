212db124eccd1fc8307331d41199e6e639a17e4c
Kevin Huo <kwhuo68@gmail.com>
2016-11-27 13:42:29 -0500

    temporary script and Rmd file

---
+++ script.R
@@ -0,0 +1,8 @@
+rm(list=ls()) # Remove all the existing variables
+setwd("~/Documents/Penn 2016-2017/WUDAC/EA_games")
+
+results = read.csv("focal_results.csv")
+original_users = read.csv("focal_users.csv")
+
+#Code cleaning
+
